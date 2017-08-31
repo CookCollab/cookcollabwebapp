cede2ac17df47efb08e432064777be7f08a68200
Michael Correia <MichaelPatrickCorreia@gmail.com>
2017-08-31 15:41:17 -0400

    Added support for event schema

---
+++ src/main/java/cookcollab/com/cookcollab/web/EventController.java
@@ -0,0 +1,33 @@
+package cookcollab.com.cookcollab.web;
+
+import cookcollab.com.cookcollab.data.entity.Event;
+import cookcollab.com.cookcollab.data.repo.EventRepository;
+import java.sql.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EventController {
+
+    private EventRepository eventRepo;
+
+    @Autowired
+    public EventController(EventRepository eventRepo) {
+        this.eventRepo = eventRepo;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/event/{id}")
+    public Event getEvent(@PathVariable(value="id") long id){
+        return eventRepo.findByEventID(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/event")
+    public List<Event> getAllEvents(){
+        return (List<Event>) eventRepo.findAll();
+    }
+}
